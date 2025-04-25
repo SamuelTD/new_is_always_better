9d28b0bc4d195c0c1cbeea739d5c4a2350bfd576
Samuel Thorez-Debrucq <S.Thorez@hotmail.fr>
2025-04-25 13:43:20 +0200

    feature : modify models (added shap_values and predicted affluence_2)

--- django/NewIsAlwaysBetter/app/models.py
+++ django/NewIsAlwaysBetter/app/models.py
@@ -9,6 +9,10 @@ class Movie(models.Model):
     picture_url = models.CharField(max_length=256, default="")
     synopsis = models.CharField(max_length=1500, default="")
     predicted_affluence = models.PositiveIntegerField(verbose_name="Affluence prédite", default=0)
+    predicted_affluence_2 = models.PositiveIntegerField(verbose_name="Affluence prédite 2", default=0)
     real_affluence = models.PositiveBigIntegerField(verbose_name="Affluence réelle", default=0)
     date = models.DateField(default=now)
+    shap_values = models.TextField(verbose_name="Shap values", default="")
+    shap_values_2 = models.TextField(verbose_name="Shap values 2", default="")
+    
     
